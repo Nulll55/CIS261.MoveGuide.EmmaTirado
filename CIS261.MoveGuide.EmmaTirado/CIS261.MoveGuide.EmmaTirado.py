761afa52a9c34df7e34844c0cba1d27d46df6afe
username <useremail>
2025-04-29 23:23:08 -0700

    Add project files.

---
+++ CIS261.MoveGuide.EmmaTirado/CIS261.MoveGuide.EmmaTirado.py
@@ -0,0 +1,15 @@
+# Cource CIS261 Week 4 Lab: Movie Guide Part 1
+# Emma Kailani Tirado - 04.30.2025
+
+# The Movie List program
+
+# COMMAND MENU - make while loop to check for valid commands (lis = invalid)
+# While (user input is not == ['list', 'add', 'del', 'exit'].lower()
+# list
+# add ( we will use the append() function here to add to the list)
+# del
+# exit 
+
+
+
+# when user input = 'end' .lower() "Bye!"
